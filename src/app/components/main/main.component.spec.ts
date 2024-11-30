b049a272b846805bd1630e330a9104997660bce1
christian <cristianyamith@hotmail.com>
2024-11-30 07:58:13 -0500

    feat: jasmine

--- src/app/components/main/main.component.spec.ts
+++ src/app/components/main/main.component.spec.ts
@@ -8,9 +8,8 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainComponent]
-    })
-    .compileComponents();
+      imports: [MainComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,38 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should calculate age correctly if date is valid and user is old enough', () => {
+    component.fechaNacimiento = '2000-01-01';
+    component.calcularEdad();
+
+    expect(component.edadCalculada).toBeGreaterThan(15);
+    expect(component.mensajeErrorFecha).toBe('');
+  });
+
+  it('should show an error if the user is younger than the minimum age', () => {
+    component.fechaNacimiento = '2015-01-01';
+    component.calcularEdad();
+
+    expect(component.edadCalculada).toBe(0);
+    expect(component.mensajeErrorFecha).toBe(
+      `Debe tener al menos ${component.ANIOS_MINIMO} años para continuar.`
+    );
+  });
+
+  it('should show error messages for missing fields', () => {
+    component.fechaNacimiento = '';
+    component.descripcionSexo = '';
+    component.programaAcademico = '';
+
+    component.validarCampos();
+
+    expect(component.mensajeErrorFecha).toBe(
+      'La fecha de nacimiento es obligatoria.'
+    );
+    expect(component.mensajeErrorSexo).toBe('Debe seleccionar un sexo.');
+    expect(component.mensajeErrorPrograma).toBe(
+      'Debe seleccionar un programa académico.'
+    );
   });
 });
